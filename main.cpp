aa32ea15ea16e48b4c3779ad14e33a1708ac0749
Konrad Turzyniecki <k.turzyniecki@gloo.pl>
2020-03-20 23:16:48 +0100

    Other branch

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "New branch!" << std::endl;
-    std::cout << "Next commit!" << std::endl;
+    std::cout << "Next commitm!" << std::endl;
+    std::cout << "Other branch!" << std::endl;
     return 0;
 }
